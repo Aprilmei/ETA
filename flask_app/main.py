b9f68587fbe97b47c5cde9e3cbcceb3d21d341da
conanmartin <conanmartin@gmail.com>
2017-07-06 17:48:12 +0100

    Weather API call set up for window.onload. Info not currently being displayed. Destinations now upate with stations reachable from the selected origin. Routes update after a destination is selected with all routes connecting the two stops. To be further improved once we have details of stop orders for routes. Routes display to be considered too... currently just presenting journey_pattern_id. If new origin or destination are selected, appropriate lists are wiped before adding the new stops/routes.

--- flask_app/main.py
+++ flask_app/main.py
@@ -7,7 +7,6 @@ from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine
 
 
-
 app = Flask(__name__, static_url_path='')
 
 
@@ -55,11 +54,22 @@ def get_destination_stops(stop_id):
 	engine = connect_to_database()
 	conn = engine.connect()
 	stops = []
-	rows = conn.execute("SELECT stop_address, stop_id FROM bus_stops WHERE stop_id = {} ORDER BY stop_address;".format(stop_id))
+	rows = conn.execute("select * from eta.bus_stops where stop_id in (select distinct stop_id from eta.routes where journey_pattern in (select journey_pattern from eta.routes where stop_id = {}) and stop_id != {}) order by stop_address;".format(stop_id, stop_id))
 	for row in rows:
 		stops.append(dict(row))
 
 	return jsonify(stops=stops)
+
+@app.route("/possible_routes/<int:origin_id>/<int:destination_id>")
+def get_possible_routes(origin_id, destination_id):
+	engine = connect_to_database()
+	conn = engine.connect()
+	routes = []
+	rows = conn.execute("SELECT journey_pattern FROM eta.routes Where stop_id = {} and journey_pattern in ( SELECT journey_pattern FROM eta.routes Where stop_id = {});".format(origin_id, destination_id))
+	for row in rows:
+		routes.append(dict(row))
+
+	return jsonify(routes=routes)
 
 
 if __name__ == '__main__':
