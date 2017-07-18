d3f64882003e2c3252553cdb48741ef9f691cc27
PamelaKelly <pamelakelly7@gmail.com>
2017-07-18 17:03:23 +0100

    updated database script

--- database_scripts/database.py
+++ database_scripts/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR
 import json
+import os
 
 Base = declarative_base()
 
@@ -26,6 +27,20 @@ class Stop(Base):
         year=%s)>
         """%(self.stop_id, self.stop_address, self.latitude,
              self.longitude, self.year)
+
+class Route(Base):
+    __tablename__ = 'routes'
+
+    journey_pattern = Column(String, primary_key=True, nullable=False)
+    stop_id = Column(Integer, primary_key=True, nullable=False)
+    position_on_route = Column(Integer)
+
+    def __repr__(self):
+        return """
+        <Route=(journey_pattern=%s,
+        stop_id=%s,
+        position_on_route=%s>
+        """%(self.journey_pattern, self.stop_id, self.position_on_route)
 
 class database_setup():
 
@@ -66,17 +81,11 @@ class database_setup():
         return self.__DB_NAME
 
     def db_engine(self):
-        try:
-            fh = open(self.__password_file)
-            self.__PASSWORD = fh.readline().strip()
-            db_engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(self.__USERNAME, self.__PASSWORD,
+        fh = open(self.__password_file)
+        self.__PASSWORD = fh.readline().strip()
+        db_engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(self.__USERNAME, self.__PASSWORD,
                 self.__URI, self.__PORT, self.__DB_NAME))
-            return db_engine
-        except Exception as e:
-            print("Error in the database class - in the db_engine() method")
-            print("Error Type: ", type(e))
-            print("Error Details: ", e)
-            return 0
+        return db_engine
 
     def __str__(self):
         return "Database Name: " + self.__DB_NAME
@@ -102,22 +111,26 @@ class database_setup():
             return full_table_description
             engine.dispose()
 
-    def create_table(self, table_name, columns, primary_key):
+    def create_table(self, table_name, columns, primary_key, foreign_key, ref_table):
         try:
             engine = self.db_engine()
             sql = "CREATE TABLE IF NOT EXISTS " + table_name + " ("
-            for i in range(len(columns)):
+            for i in range(len(columns)-1):
                 sql += (str(columns[i]) + ", ")
-            sql += ("PRIMARY KEY (" + primary_key + "));")
+            sql += (columns[len(columns)-1] + ") PRIMARY KEY " + primary_key)
+            if foreign_key != None:
+                sql += (", FOREIGN KEY (" + foreign_key + ") REFERENCES " + ref_table + "(" + foreign_key + "));")
+            else:
+                sql += ");"
             engine.execute(sql)
+            return("Table " + table_name + " created.")
         except Exception as e:
             print("Error in the create_table() function")
             print("Error Type: ", type(e))
             print("Error Details: ", e)
-            return 0
+            return(type(e))
         finally:
             engine.dispose()
-            return 1
 
     def drop_table(self, table):
         try:
@@ -128,35 +141,15 @@ class database_setup():
             print("Error in the drop_table() function")
             print("Error type: ", type(e))
             print("Error details: ", e)
-        finally:
-            engine.dispose()
-
-    def insert(self, table, columns, values):
-        try:
-            engine = self.db_engine()
-            sql = "INSERT INTO `" + str(table) + "` ("
-            for i in range(len(columns)-1):
-                sql += (str(columns[i]) + ", ")
-            sql += (columns[len(columns)-1] + ") VALUES (")
-            for i in range(len(values)-1):
-                sql += str(values[i]) + ", "
-            sql += (str(values[len(values)-1]) + ");")
-            print(sql)
-            engine.execute(sql)
-            print("success")
-        except Exception as e:
-            print("Error in the insert() function")
-            print("Error Type: ", type(e))
-            print("Error Details: ", e)
-
+            # throw e
         finally:
             engine.dispose()
 
     def delete(self, table, column, value):
         pass
 
-    def insert_stop(self, stop_info, year):
-        db = database_setup("db_password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
+    def insert_stops(self, stop_info, year):
+        db = database_setup("password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
         engine = db.db_engine()
         Session = sessionmaker(bind=engine)
         session = Session()
@@ -164,15 +157,14 @@ class database_setup():
         with open(stop_info) as f:
             stops_json = json.load(f)
         for stop in stops_json:
-            print("TYPE OF STOP", type(stop))
             try:
-                stop = Stop(stop_id=stop,
+                stop_obj = Stop(stop_id=stop,
                             stop_address=stops_json[stop]['stop_address'],
                             latitude=stops_json[stop]['latitude'],
                             longitude=stops_json[stop]['longitude'],
                             year=year)
 
-                session.add(stop)
+                session.add(stop_obj)
                 session.commit()
             except Exception as e:
                 print("Error in the insert_stop() function")
@@ -180,6 +172,39 @@ class database_setup():
                 print("Error details: ", e)
                 session.rollback()
                 continue
+        session.close()
+        engine.dispose()
+
+    def insert_routes(self, route_info):
+        db = database_setup("password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
+        engine = db.db_engine()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        with open(route_info) as f:
+            routes_json = json.load(f)
+        count_routes = 0
+        count_stops = 0
+        for route in routes_json:
+            count_routes += 1
+            print("Route: ", route)
+            stops = routes_json[route]["stops"]
+            for stop in stops:
+                try:
+                    count_stops += 1
+                    print("Stops: ", stop)
+                    route_obj = Route(journey_pattern=route,
+                                  stop_id=stop)
+
+                    session.add(route_obj)
+                    session.commit()
+
+                except Exception as e:
+                    print("Error in the insert_routes function")
+                    print("Error Type: ", type(e))
+                    print("Error Details: ", e)
+                    session.rollback()
+                    continue
         session.close()
         engine.dispose()
 
@@ -195,27 +220,3 @@ class database_setup():
         finally:
             return result
             engine.dispose()
-
-def static_stops_table():
-    try:
-        db_obj = database_setup("db_password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
-        db_obj.create_table('bus_stops', ['stop_id INT NOT NULL', 'stop_address VARCHAR(200) NOT NULL',
-                                          'latitude FLOAT NOT NULL', 'longitude FLOAT NOT NULL', 'year INT NOT NULL'],
-                            "stop_id, latitude, longitude")
-    except Exception as e:
-        print("Error in the static_stops_tables() function")
-        print("Error Type: ", type(e))
-        print("Error Details: ", e)
-        return 0
-
-def populate_stops_table():
-    """ Function to repopulate static stops table with 2012 and 2017 stops, in the case of loss of data """
-    # Currently the data is pulled from a local file - should this data be backed up somewhere else ? in the cloud ?
-    db = database_setup("db_password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
-    db.insert_stop('stop_objects_2012.txt', 2012)
-    db.insert_stop('stop_info_2017.txt', 2017)
-
-
-eta_db = database_setup("db_password.txt", "eta.cb0ofqejduea.eu-west-1.rds.amazonaws.com", "3306", "eta", "eta")
-eta_db.get_tables(tables=None)
-eta_db.create_table('test', [], 'column 1')
