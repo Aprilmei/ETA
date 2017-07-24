5de7573c71410ee5faf3d6efc6cbcbdf7954d42d
Andrew Cameron <andrew.cameron@protonmail.com>
2017-07-25 00:43:48 +0100

    moved app declaration out of `if __name__...` block

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
+app = Flask(__name__)
+from routes import *
+
 if __name__ == '__main__':
-    app = Flask(__name__)
-    from routes import *
     app.run(host='0.0.0.0', port=5000, debug=True)
