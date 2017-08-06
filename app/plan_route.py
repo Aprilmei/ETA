0d0cd9c8de681243970ebcc77282caf94c5ecd0a
Andrew Cameron <andrew.cameron@protonmail.com>
2017-08-06 20:08:17 +0100

    refactored plan_route.py => parse_route()

--- app/plan_route.py
+++ app/plan_route.py
@@ -8,12 +8,42 @@ from data_loader import ALL_STOPS_TREE, COORD_STOPS, GRAPH, LINES, STOP_COORDS
 from main import log
 
 
-def k_nearest_stop_coords(lat: float, 
-                          lon: float, 
-                          k: int = 1, 
-                          tree: cKDTree = ALL_STOPS_TREE)\
-                          -> List[Tuple[float, float]]:
-    """Queries TREE for the K nearest neighbours for the specified lat and lon.
+def find_routes(origin: Tuple[float, float],
+                destination: Tuple[float, float],
+                max_walk: int = 500) -> List[List[dict]]:
+
+    origin_stops = k_nearest_stop_coords(*origin, k=10)
+    destination_stops = k_nearest_stop_coords(*destination, k=10)
+
+    # filter stops within walking distance
+    origin_stops = [os for os in origin_stops
+                    if distance(origin, os).meters <= max_walk]
+
+    destination_stops = [ds for ds in destination_stops
+                         if distance(destination, ds).meters <= max_walk]
+
+    transit_options = [
+        journey_transits(stop_id(origin), stop_id(dest))
+        for origin in origin_stops
+        for dest in destination_stops
+    ]
+
+    # only keep the options with fewest number of changes
+    fewest_changes = len(min(transit_options, key=len))
+
+    transit_options = [t for t in transit_options
+                       if len(t) == fewest_changes]
+
+    return [list(parse_route(t)) for t in transit_options]
+
+
+def k_nearest_stop_coords(lat: float,
+                          lon: float,
+                          k: int=1,
+                          tree: cKDTree=ALL_STOPS_TREE)\
+        -> List[Tuple[float, float]]:
+    """
+    Queries TREE for the K nearest neighbours for the specified lat and lon.
     @returns : [ (float(lat), float(lon)) , ...]
     """
 
@@ -29,19 +59,17 @@ def k_nearest_stop_coords(lat: float,
     return results
 
 
-def stop_id_for_coords(coords: Tuple[float, float],
-                       lookup_dict: dict = COORD_STOPS) -> str:
+def stop_id(coords: Tuple[float, float],
+            lookup_dict: dict=COORD_STOPS) -> str:
     """Takes coords and returns the corresponding Stop Id."""
     return lookup_dict[coords]
 
 
 def journey_transits(origin_stop_id: str,
                      destination_stop_id: str,
-                     max_changes: Optional[int] = None,
-                     graph: nx.MultiGraph = GRAPH) -> List[str]:
-    """Takes two stop IDs and finds the routes requiring fewest changes
-    between them.
-
+                     max_changes: Optional[int]=None,
+                     graph: nx.MultiGraph=GRAPH) -> List[str]:
+    """Takes two stop IDs and finds the routes requiring fewest changes between them.
     Returns a list of stops on the journey, including origin and destination
     """
 
@@ -49,16 +77,14 @@ def journey_transits(origin_stop_id: str,
 
     if max_changes and len(changes) - 2 > max_changes:
         return []
-
     return changes
 
 
 def lines_connecting_stops(origin_stop_id: str,
                            destination_stop_id: str,
-                           graph: nx.MultiGraph = GRAPH) -> List[str]:
+                           graph: nx.MultiGraph=GRAPH) -> List[str]:
     """Takes a origin stop ID and destination stop ID, and returns a list of
-    lines that go directly from origin to destination. 
-
+    lines that go directly from origin to destination.
     Assumes no bus changes required (use necessary_stop_changes() first if needed)
     """
     results = []
@@ -71,50 +97,7 @@ def lines_connecting_stops(origin_stop_id: str,
     return results
 
 
-def find_routes(origin: Tuple[float, float],
-                destination: Tuple[float, float],
-                max_walk: int = 500) -> List[List[dict]]:
-
-    """Takes two (lat, lon) coordinates, and finds the easiest route between them.
-    TODO: This is damn ugly, needs refactoring
-    """
-
-    origin_stops = k_nearest_stop_coords(*origin, k=10)
-    destination_stops = k_nearest_stop_coords(*destination, k=10)
-
-    # get rid of stops not within walking distance
-    origin_stops = [os for os in origin_stops
-                    if distance(origin, os).meters <= max_walk]
-
-    destination_stops = [ds for ds in destination_stops
-                         if distance(destination, ds).meters <= max_walk]
-
-    journey_options = []
-    for origin in origin_stops:
-        for destination in destination_stops:
-
-            # This will give us the stops in a journey required for fewest
-            # number of changes
-            journey_options.append(
-                journey_transits(stop_id_for_coords(origin),
-                                 stop_id_for_coords(destination)))
-
-    # only keep the options with fewest number of changes
-    fewest_changes = float('inf')
-    for opt in journey_options:
-        if len(opt) < fewest_changes:
-            fewest_changes = len(opt)
-
-    journey_options = [opt for opt in journey_options
-                       if len(opt) == fewest_changes]
-
-    journey_options = [list(parse_route(opt)) for opt in journey_options]
-
-    log.debug(journey_options[0])
-    return journey_options
-
-
-def parse_route(changes: List[str], graph: nx.MultiGraph = GRAPH)\
+def parse_route(changes: List[str], graph: nx.MultiGraph=GRAPH)\
         -> Generator[Dict[str, Any], None, None]:
     """
     @yields : {busses : list(of_bus_lines), board: str(stop_id), deboard: str(stop_id)}
